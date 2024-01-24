97ee6a86cd5fd33c27a5bf71365db939df84225f
Andy Johnson <andy.johnson.1993@hotmail.co.uk>
2024-01-24 11:44:11 +0000

    Added name, age and number to choose from 1 to 10

--- random_message.js
+++ random_message.js
@@ -1,25 +1,33 @@
 
 const randomJoke = () => {
-    let joke = Math.floor(Math.random() * 8)
+    let joke = Math.floor(Math.random() * 9)
     if (joke === 1 || joke === 8) {
-        console.log('Two men walk into a bar. Ouch');
+        return ('Two men walk into a bar. Ouch');
     } else if (joke === 2 || joke === 7) {
-        console.log('What is brown and sticky? A stick');
+        return ('What is brown and sticky? A stick');
     } else if (joke === 3 || joke === 6) {
-        console.log('Why did the cow stop? It couldn\'t moooove');
+        return ('Why did the cow stop? It couldn\'t moooove');
     } else if (joke === 4 || joke === 5) {
-        console.log('What do you call a Spanish man with no legs? Gracias');
+        return ('What do you call a Spanish man with no legs? Gracias');
     }
 };
 
 // randomJoke();
 
-const chooseANumber = num => {
+const chooseANumber = (name, age, num) => {
     if (num >= 1 && num <= 10) {
-        return randomJoke();
+        return `Welcome to the fun ${name}. Your random joke of the day is: ${randomJoke()}. You're never too old to enjoy a joke, even at the grand age of ${age}.`;
     } else {
         console.log('Please choose a number between 1 & 10');
     }
 };
+
+console.log(chooseANumber('Andy', 30, 3));
+
+// TRYING TO ADD DATA TO OBJECT
+
+
+
+
 
 
