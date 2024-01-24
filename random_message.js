08c2cf1cfa9e85fe3c492145253c1ea0e78c1284
Andy Johnson <andy.johnson.1993@hotmail.co.uk>
2024-01-24 10:49:38 +0000

    Second commit, jokes print to console

--- random_message.js
+++ random_message.js
@@ -1,16 +1,18 @@
 
 const randomJoke = () => {
-    let message = Math.floor(Math.random * 4)
-    if (joke === 1) {
-        return 'Two men walk into a bar. Ouch';
-    } else if (joke === 2) {
-        return 'What is brown and sticky? A stick';
-    } else if (joke === 3) {
-        return 'Why did the cow stop? It couldn\'t moooove';
-    } else if (joke === 4) {
-        return 'What do you call a Spanish man with no legs? Gracias';
+    let joke = Math.floor(Math.random() * 8)
+    if (joke === 1 || joke === 8) {
+        console.log('Two men walk into a bar. Ouch');
+    } else if (joke === 2 || joke === 7) {
+        console.log('What is brown and sticky? A stick');
+    } else if (joke === 3 || joke === 6) {
+        console.log('Why did the cow stop? It couldn\'t moooove');
+    } else if (joke === 4 || joke === 5) {
+        console.log('What do you call a Spanish man with no legs? Gracias');
     }
 };
+
+randomJoke();
 
 
 
