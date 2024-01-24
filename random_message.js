3c94d3d5d3e7bb54d1af0117d6304072171d164b
Andy Johnson <andy.johnson.1993@hotmail.co.uk>
2024-01-24 10:33:21 +0000

    First commit

---
+++ random_message.js
@@ -0,0 +1,17 @@
+
+const randomJoke = () => {
+    let message = Math.floor(Math.random * 4)
+    if (joke === 1) {
+        return 'Two men walk into a bar. Ouch';
+    } else if (joke === 2) {
+        return 'What is brown and sticky? A stick';
+    } else if (joke === 3) {
+        return 'Why did the cow stop? It couldn\'t moooove';
+    } else if (joke === 4) {
+        return 'What do you call a Spanish man with no legs? Gracias';
+    }
+};
+
+
+
+
